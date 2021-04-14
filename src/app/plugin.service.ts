783b464219325b1fad315493792e2ac169dd0f3a
Khadija <khadija@curvetomorrow.com.au>
2021-04-14 22:51:24 +0530

    changed the bundle id and app name

--- src/app/plugin.service.ts
+++ src/app/plugin.service.ts
@@ -10,17 +10,8 @@ export class PluginService extends IonicNativePlugin {
   static repo = 'https://github.com/KhadijaDaruwala/Cordova-Plugin-Example.git';
   static platforms = ['iOS']
 
-  // add(num1, num2): Promise<any> {
-  //   const x = cordova(this, 'add', {}, [{ param1: num1, param2: num2 }])
-  //   return x;
-  // }
-
-  // subtract(num1, num2): Promise<any> {
-  //   const x = cordova(this, 'subtract', {}, [{ param1: num1, param2: num2 }])
-  //   return x;
-  // }
-
   startRecording(): Promise<any> {
+    console.log('recording called')
     const x = cordova(this, 'startRecording', {}, [])
     return x;
   }
